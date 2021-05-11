3e7475e3bbf815ebe4aa59da11cfc6c0ebc7dcc1
Alex <alex.bidarto@utem.cl>
2021-05-11 19:07:26 -0400

    se agregó Columnas nombre nodo

--- funciones/grafo.py
+++ funciones/grafo.py
@@ -1,12 +1,14 @@
 import random
-d=2;
+d=4;
 r1=1;
 r2=4;
+
+
 
 a=[[0 for x in range(d)] for y in range(d)]
 for i in range (0,d):
     for j in range (0,d):
-        print("Ingrese el valor",i,",",j)
+        print("Ingrese el valor",chr(97+i),",",chr(97+j))
         a[i][j]=int(input())
         #a[i][j]=int(random.randint(r1,r2))
         
