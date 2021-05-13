a802d3ad83370c3609bb4791dc6e0a3efcb3bc41
BlazeKl <felipe.perezc@utem.cl>
2021-05-13 17:25:26 -0400

    crear aristas solo a vertices por click derecho

--- gui/ventana.py
+++ gui/ventana.py
@@ -4,19 +4,22 @@ from tkinter import *
 global click
 
 def d_nodo(event):
-    global x,y
+    global x,y,nodos
     print("click izquierdo")
     x = event.x
     y = event.y
     print("(",+x,",",+y,")")
-    lienzo.create_oval(x-10,y-10,x+10,y+10,fill='black')
+    vert = lienzo.create_oval(x-10,y-10,x+10,y+10,fill='black',activeoutline='green',activewidth=3,tags=nodos)
+    lienzo.tag_bind(vert,'<Button-3>',d_arista)
+    nodos += 1
 
 def d_arista(event):
     global x,y
     global click
     print("click derecho")
     if click:
-        lienzo.create_line(x,y,event.x,event.y,fill='black',width=2)
+        arist = lienzo.create_line(x,y,event.x,event.y,fill='black',width=2)
+        lienzo.tag_lower(arist)
         print("(",+event.x,",",+event.y,")")
         click=0
     else:
@@ -25,11 +28,10 @@ def d_arista(event):
         print("(",+x,",",+y,")")
         click=1
 
-
+nodos=0
 click=0
 ventana = Tk()
 lienzo = Canvas(ventana,width=640,height=480,background='light blue')
 lienzo.grid(row=0,column=0)
 lienzo.bind('<Button-1>',d_nodo)
-lienzo.bind('<Button-3>',d_arista)
 ventana.mainloop()
