3c4cd8063d67745a1d1b18011849c39b6d3e9a5e
BlazeKl <felipe.perezc@utem.cl>
2021-05-28 18:22:11 -0400

    prueba aristas dirigida

---
+++ funciones/gradosaristas.py
@@ -0,0 +1,8 @@
+# X es el arreglo bidimensional (matriz)
+# n es el largo del arreglo, matriz n*n cuadrada
+
+
+def gradosgrafo(x,n):
+    for i in range(0,n):
+        for j in range(0,n):
+            if i == 1:
